cd554a5b73c22fee0beaee2610f336b481b3749b
santiagosayshey <chaucted@gmail.com>
2023-03-02 09:53:56 +1030

    a

--- Referee.cpp
+++ Referee.cpp
@@ -12,12 +12,15 @@ Referee::~Referee()
 
 Player* Referee::refGame(Player* player1, Player* player2)
 {
-    if (player1->move == 'P' || player1->move == 'p')
-    {
-      return player1;
-    }
-    else
-    {
-      return player2;
-    }
+  player1->makeMove();
+  player2->makeMove();
+
+  if (player1->move == 'P' || player1->move == 'p')
+  {
+    return player1;
+  }
+  else
+  {
+    return player2;
+  }
 }
--- main.cpp
+++ main.cpp
@@ -9,14 +9,15 @@ int main()
   Player* p2 = new Computer();
   Referee* r = new Referee();
 
-  p1->makeMove();
+/*   p1->makeMove();
 
   if (p1->move == 'R'|| p1->move == 'r')
   {
     std::cout << "Tie" << std::endl;
     return 0;
   }
-  p2->makeMove();
+
+  p2->makeMove(); */
 
   Player* winner;
   winner = r->refGame(p1,p2);
