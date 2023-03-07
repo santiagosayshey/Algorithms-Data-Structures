529bcfb3b976a5a398bdf0aae8a7f6c7b6cb40d4
Samuel Chau <chaucted@gmail.com>
2023-03-07 17:50:05 +1030

    a

--- Zombie.cpp
+++ Zombie.cpp
@@ -3,8 +3,8 @@
 Zombie::Zombie()
 {
     this->name="Zombie";
-    this->losesTo.push_back("Ninja");
     this->losesTo.push_back("Robot");
+    this->losesTo.push_back("Ninja");
     this->winsTo.push_back("Pirate");
     this->winsTo.push_back("Monkey");
 }
