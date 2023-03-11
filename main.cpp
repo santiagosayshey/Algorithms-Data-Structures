7430849c92db4d8f82f41b5a08176d39d3986123
Samuel Chau <chaucted@gmail.com>
2023-03-12 07:01:09 +1030

    did some recursion exercises

--- main.cpp
+++ main.cpp
@@ -1,3 +1,25 @@
+#include <iostream>
+
+int sum(int n)
+{
+    if (n<2)
+        return 1;
+    return n+sum(n-1);
+}
+
+int power(int x, int n)
+{
+    if (n<1)
+        return 1;
+    return x*power(x,n-1);
+}
+
+int sumIntDigits(int n)
+{
+    if 
+}
+
 int main() {
-  
+    std::cout << power(9,9) << std::endl;
+    return 0;
 }
